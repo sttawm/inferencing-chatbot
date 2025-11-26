d215bc2833d93fb4106ca9a25e45a47df773f33f
sttawm <mmww88wwmm@gmail.com>
2025-11-26 15:57:26 -0800

    Shadcn demo now running

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { Thread } from "@/components/assistant-ui/thread";
-import { ThreadList } from "@/components/assistant-ui/thread-list";
+import { Shadcn } from "@/components/shadcn/Shadcn";
 
 export default function Home() {
-  return (
-    <main className="grid h-dvh grid-cols-[200px,1fr] gap-4 p-4">
-      <ThreadList />
-      <Thread />
-    </main>
-  );
+  return <Shadcn />;
 }
